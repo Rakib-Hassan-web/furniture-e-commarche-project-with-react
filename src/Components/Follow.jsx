2ef22d631e5f97086b22f55d2d4270ad38714392
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-08-18 01:22:34 +0600

    follow section done

--- src/Components/Follow.jsx
+++ src/Components/Follow.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const Follow = () => {
-  return (
-    <>
-    
-    <section id='follow' className='bg-[#FAF4F4] opacity-50 py-32'>
-        
-        
-        
-        
-        
-        </section></>
-  )
+return (
+<>
+
+    <section id='follow' className='bg-[#faf4f4a4]  py-32'>
+
+        <div className="container">
+
+            <h2 className='text-[60px] font-main font-bold text-black text-center'>Our Instagram</h2>
+            <h3 className='text-[20px] font-main font-bold text-black text-center mb-9'>Follow our store on Instagram</h3>
+
+<div className='text-center'>
+            <Link className='text-[20px] py-[17px] px-[75px]  font-normal font-main text-[#000] text-center border rounded-full bg-pink-100 '>Order
+            Now</Link>
+            </div>
+
+        </div>
+
+
+
+
+    </section>
+</>
+)
 }
 
 export default Follow
