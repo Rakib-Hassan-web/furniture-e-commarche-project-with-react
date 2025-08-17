5a73c0a858f6ddf4cd29b6648dda10994aa129c2
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-08-18 00:24:20 +0600

    picks section almsot

---
+++ src/Components/Single_picks.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Single_picks = () => {
+  return (
+    <>
+    
+    <section className='mt-[65px]'>
+
+
+<div className='w-[287px] pb-3 shadow-[0px_10px_20px_0px_rgba(0,_0,_0,_0.1)]'>
+    <img src="asdf" alt="Products" className='w-full h-[287px] bg-gray-100' />
+    <h3 className='text-[16px] font-medium font-main text-[#000]  mt-3.5 pl-2'>Product Title</h3>
+    <p className='text-[24px] font-medium font-main text-[#000]  mt-3.5 pl-2'>Product Price</p>
+
+</div>
+
+
+
+
+
+    </section>
+    
+    
+    
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default Single_picks
--- src/Components/Top_Picks.jsx
+++ src/Components/Top_Picks.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Single_picks from './Single_picks'
 
 const Top_Picks = () => {
   return (
@@ -9,6 +10,17 @@ const Top_Picks = () => {
         <div className="container">
             <h2 className='text-[36px] font-medium font-main text-black text-center'>Top Picks For You</h2>
             <p className='text-[16px] font-medium font-main text-[#9F9F9F] text-center mt-3.5'>Find a bright ideal to suit your taste with our great selection of suspension, floor and table lights.</p>
+
+
+<div className='flex items-center justify-between'>
+    <Single_picks/>
+    <Single_picks/>
+    <Single_picks/>
+    <Single_picks/>
+</div>
+
+
+
         </div>
     </section>
     
