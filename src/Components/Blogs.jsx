b9214c7e3cb7ee1fb34102e1188fdad08f0a4705
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-08-18 00:43:36 +0600

    bloge section st

---
+++ src/Components/Blogs.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Blogs = () => {
+  return (
+    <>
+    
+    
+    <section id='blog' className=''>
+        <div className="container">
+
+
+
+
+            
+        </div>
+    </section>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default Blogs
