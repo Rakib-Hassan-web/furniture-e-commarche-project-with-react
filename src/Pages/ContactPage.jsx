026f5b63d7309d01d47a2ccbce6393f37204ee9c
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-08-18 22:43:44 +0600

    contact page header

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -4,7 +4,15 @@ const ContactPage = () => {
   return (
     <>
     
-    
+    <section id='contactPage'>
+        <div className="container pt-[98px] bg-white pb-18">
+            {/* ---header--- */}
+            <h2 className='text-[36px] font-medium font-main text-black text-center'>Get In Touch With Us</h2>
+            <div className='flex justify-center'>
+            <p className='w-[644px] text-[16px] font-medium font-main text-[#9F9F9F] text-center mt-3.5'>For More Information About Our Product & Services. Please Feel Free To Drop Us An Email. Our Staff Always Be There To Help You Out. Do Not Hesitate!</p>
+            </div>
+        </div>
+    </section>
     
     
     
