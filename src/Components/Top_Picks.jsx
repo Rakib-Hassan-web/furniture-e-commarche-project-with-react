b0f6105082745e894089da9d12db3463162e9aa7
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-08-31 16:31:19 +0600

    api er data then er modhhe jachhe

--- src/Components/Top_Picks.jsx
+++ src/Components/Top_Picks.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Single_picks from './Single_picks'
 import { Link } from 'react-router'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import axios from 'axios';
 const Top_Picks = () => {
+
+
+
+
+
+
+
+    const [product ,  setproduct ] =useState('')
+    
+
+
+    useEffect(()=>{
+      axios.get('https://api.escuelajs.co/api/v1/categories')
+      .then((res) => console.log(res.data))  
+      .catch((error)=> console.log('error dichhe'))
+    },[])
+
+   
 return (
 <>
 
