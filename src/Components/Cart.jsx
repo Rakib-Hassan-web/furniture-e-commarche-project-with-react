fd5bb10aa5d94f68bb3b50c10cb717d95495f201
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-09-02 17:14:17 +0600

    subtotal design

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -13,17 +13,17 @@ return (
 
 
 
-  <div  onClick={closeDiv} className=" h-screen w-full fixed top-0 right-0 z-10">
-   
-    
+  <div onClick={closeDiv} className=" h-screen w-full fixed top-0 right-0 z-10">
+
+
 
     <div className='w-[417px] h-screen bg-white ml-auto px-[30px] pt-[48px]'>
       <div className='flex items-center justify-between'>
         <h1 className=' font-main text-black text-[24px] font-semibold'>Shopping Cart</h1>
 
-       <div>
-        {closecart}
-       </div>
+        <div>
+          {closecart}
+        </div>
 
       </div>
 
@@ -32,24 +32,26 @@ return (
         <div className='mt-[42px] flex items-center justify-between'>
           <img src="ad" alt="product image" className='w-[104px]  h-[105px] bg-gray-200 rounded-[10px] ' />
           <div>
-          <h2 className='text-base text-black  font-normal font-main'>Asgaard sofa</h2>
-          <p className='text-[14px] mt-2 font-main text-[#B88E2F] font-medium'>Rs. 250,000.00</p>
+            <h2 className='text-base text-black  font-normal font-main'>Asgaard sofa</h2>
+            <p className='text-[14px] mt-2 font-main text-[#B88E2F] font-medium'>Rs. 250,000.00</p>
           </div>
           <div>
-            <IoCloseCircleSharp  className='text-[#9F9F9F] text-4xl cursor-pointer '/>
+            <IoCloseCircleSharp className='text-[#9F9F9F] text-4xl cursor-pointer ' />
 
           </div>
         </div>
-{/* --check out----- */}
+        {/* --check out----- */}
+
         <div>
-          <div>
-            <h2 className='text-base font-main font-normal text-black' >Subtotal</h2>
+          <div className='flex items-center justify-between'>
+            <h2 className='text-base font-main font-normal text-black'>Subtotal</h2>
             <h2 className='text-base font-main font-semibold text-[#B88E2F]'>Rs. 250,000.00</h2>
           </div>
 
-<div>
+          <div className='w-full border-b  border-1 border-[#D9D9D9] flex  m-auto mt-[26px]'></div>
+          <div>
 
-</div>
+          </div>
         </div>
       </div>
     </div>
