a86df5a92d5380003081b221f69c9d99b1ff8512
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-09-03 00:55:00 +0600

    icon adden need to solve

--- src/Components/Single_picks.jsx
+++ src/Components/Single_picks.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaHeart, FaShoppingCart } from 'react-icons/fa'
 
 const Single_picks = ({proimage ,protitle ,proprice ,CartClick}) => {
 return (
@@ -6,34 +7,44 @@ return (
 
     <section className='mt-[65px] mb-26'>
 
+   <div
+      onClick={CartClick}
+      className="group w-[287px] pb-3 shadow-[0px_10px_20px_0px_rgba(0,_0,_0,_0.1)] rounded-xl"
+    >
+      {/* image wrapper */}
+      <div className="relative w-full h-[287px] overflow-hidden rounded-xl">
+        <img
+          src={proimage}
+          alt="Products"
+          className="w-full h-full object-cover"
+        />
 
-     <div 
-  onClick={CartClick} 
-  className="group w-[287px] pb-3 shadow-[0px_10px_20px_0px_rgba(0,_0,_0,_0.1)] rounded-xl"
->
-  {/* image wrapper */}
-  <div className="relative w-full h-[287px] overflow-hidden rounded-xl">
-    <img 
-      src={proimage} 
-      alt="Products" 
-      className="w-full h-full object-cover"
-    />
-    {/* overlay shadow */}
-    <div className="absolute inset-0 bg-black/40 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
-  </div>
+        {/* overlay + icons */}
+        <div className="absolute inset-0 bg-black/40 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-between p-3">
+          {/* cart icon top-right */}
+          <div className="flex justify-end">
+            <button className="bg-white p-2 rounded-full shadow hover:bg-gray-200 transition">
+              <FaShoppingCart className="text-black text-lg" />
+            </button>
+          </div>
 
-  <h3
-    className="text-[16px] font-medium font-main text-[#000] mt-3.5 pl-2 w-[260px] truncate overflow-hidden whitespace-nowrap"
-  >
-    {protitle}
-  </h3>
+          {/* love icon bottom-center */}
+          <div className="flex justify-center">
+            <button className="bg-white p-2 rounded-full shadow hover:bg-gray-200 transition">
+              <FaHeart className="text-red-500 text-lg" />
+            </button>
+          </div>
+        </div>
+      </div>
 
-  <p className="text-[24px] font-medium font-main text-[#000] mt-3.5 pl-2">
-    {proprice}$
-  </p>
-</div>
+      <h3 className="text-[16px] font-medium font-main text-[#000] mt-3.5 pl-2 w-[260px] truncate overflow-hidden whitespace-nowrap">
+        {protitle}
+      </h3>
 
-
+      <p className="text-[24px] font-medium font-main text-[#000] mt-3.5 pl-2">
+        {proprice}$
+      </p>
+    </div>
 
 
 
