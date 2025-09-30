2382b0bd9a5fbbf86adab28dcd851d4be4ba85e8
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-10-01 01:19:10 +0600

    responcive

--- src/Components/NewArrival.jsx
+++ src/Components/NewArrival.jsx
@@ -6,17 +6,23 @@ const NewArrival = () => {
   return (
     <section id='NewArrival' className='bg-[#FFF9E5] py-14'>
       <div className="container mx-auto px-4">
-        <div className='flex flex-col md:flex-row items-center md:justify-between gap-8 md:gap-0'>
-          
+        <div className='flex flex-col md:flex-row xl:flex-row items-center justify-between gap-8 md:gap-8 xl:gap-12'>
+
           {/* Image */}
-          <div className='w-full md:w-1/2 flex justify-center md:justify-start'>
-            <img src={image} alt="Sofa Image" className='w-full max-w-[500px] md:max-w-full object-contain' />
+          <div className='w-full md:w-1/2 xl:w-1/2 flex justify-center md:justify-start'>
+            <img 
+              src={image} 
+              alt="Sofa Image" 
+              className='w-full max-w-[500px] sm:max-w-[600px] xl:max-w-[700px] object-contain' 
+            />
           </div>
 
           {/* Texts */}
-          <div className='w-full md:w-1/2 flex flex-col items-center md:items-start text-center md:text-left'>
+          <div className='w-full md:w-1/2 xl:w-1/2 flex flex-col items-center md:items-start text-center md:text-left'>
             <h3 className='text-[20px] sm:text-[24px] font-medium font-main text-[#000]'>New Arrivals</h3>
-            <h2 className='text-[32px] sm:text-[48px] font-bold font-main text-[#000] mb-6 sm:mb-8'>Asgaard sofa</h2>
+            <h2 className='text-[32px] sm:text-[48px] font-bold font-main text-[#000] mb-6 sm:mb-8'>
+              Asgaard sofa
+            </h2>
             <Link 
               className='text-[16px] sm:text-[20px] py-3 sm:py-4 px-8 sm:px-20 font-bold font-main text-[#000] border'
             >
