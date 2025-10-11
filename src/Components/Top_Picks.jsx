8ef7914dc4876f57b9082ef8332f41652b49ddcb
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-10-11 23:17:50 +0600

    conmtact page add page and processing

--- src/Components/Top_Picks.jsx
+++ src/Components/Top_Picks.jsx
@@ -26,19 +26,27 @@ const Top_Picks = () => {
     autoplaySpeed: 3000,
     responsive: [
       {
-        breakpoint: 1024, // tablet
+        breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 1,
-          dots: true,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
         }
       },
       {
-        breakpoint: 768, // mobile
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1,
-          dots: true,
+          slidesToScroll: 1
         }
       }
     ]
